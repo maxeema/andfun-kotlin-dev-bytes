69dbf047274b21d39dbab58fe8ff91cc7781cc77
Maxeem <jmax.posh@gmail.com>
2019-08-19 21:52:27 +0300

    complete Exercise: Add the VideoDao

--- app/src/main/java/maxeem/america/devbyteviewer/database/Room.kt
+++ app/src/main/java/maxeem/america/devbyteviewer/database/Room.kt
@@ -16,3 +16,19 @@
  */
 
 package maxeem.america.devbyteviewer.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+
+    @Query("SELECT * FROM videos")
+    fun getAll(): List<DatabaseVideo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+
+}
